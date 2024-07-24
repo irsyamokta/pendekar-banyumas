24ef178cdbdc691b09dc516f7ef3b12a3f5b246c
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-07-24 17:24:59 +0700

    Fix : Route edit instrumen SRQ

--- resources/views/admin/dashboard/partials/form/container/store-srq.blade.php
+++ resources/views/admin/dashboard/partials/form/container/store-srq.blade.php
@@ -7,13 +7,13 @@
                 <span class="text-md md:text-lg font-regular text-dark dark:text-primary">Pertanyaan {{ $loop->iteration }}</span>
             </div>
             <div class="flex gap-2">
-                <a href="{{ route('viewSDQ', $item->id_srq) }}"
+                <a href="{{ route('viewSRQ', $item->id_srq) }}"
                     class="edit-sdq flex justify-center items-center gap-1 text-sm font-medium w-[60px] h-[25px] md:w-[80px] md:h-[30px] cursor-pointer rounded-[30px] bg-[#D9D9D9] text-dark duration-300 ease-linear">
                     <img src="{{ asset('assets/icon/icon-edit.png') }}" alt="edit" class="w-3 h-3 md:w-4 md:h-4">
                     <span>Edit</span>
                 </a>
                 
-                <a href="{{ route('deleteSDQ', $item->id_srq) }}"
+                <a href="{{ route('deleteSRQ', $item->id_srq) }}"
                     class="delete-confirm flex justify-center items-center gap-1 text-sm font-medium w-[70px] h-[25px] md:w-[80px] md:h-[30px] cursor-pointer rounded-[30px] bg-[#D9D9D9] text-dark duration-300 ease-linear">
                     <img src="{{ asset('assets/icon/icon-delete.png') }}" alt="hapus" class="w-3 h-3 md:w-4 md:h-4">
                     <span>Hapus</span>
