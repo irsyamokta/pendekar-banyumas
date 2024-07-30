4bdc55cc2d7f47ef0081681c3418fef735a32058
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-07-30 12:39:09 +0700

    Fix : Responsive homepage all device

--- resources/views/client/page/homepage/components/hero.blade.php
+++ resources/views/client/page/homepage/components/hero.blade.php
@@ -1,14 +1,14 @@
 <section id="homepage" class="flex justify-center items-center h-[100vh] bg-gradient-to-t from-soft to-white">
     <div class="flex flex-col items-center lg:flex-row lg:justify-between mt-20 px-4 md:mx-12 md:mt-0 lg:w-full">
-        <div class="flex flex-col gap-2 lg:gap-4">
-            <h2 class="text-lg md:text-xl lg:text-3xl font-medium delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Selamat datang di</h2>
-            <h1 class="text-5xl md:text-6xl 2xl:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Pendekar Banyumas</h1>
-            <h3 class="text-sm md:text-xl lg:text-2xl delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Pendeteksian Kesehatan Mental Banyumas</h3>
-            <p class="text-sm md:text-lg lg:text-2xl font-semibold mt-5 delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">"Bersama Mewujudkan Jiwa yang Sehat"</p>
-            <a href="#about" class="bg-transparent bg-clip-border bg-gradient-to-r from-accent to-secondary text-white mt-8 lg:py-4 py-2 px-2 rounded-[30px] w-40 md:w-45 md:h-10 lg:h-15 lg:w-60 text-xs md:text-sm lg:text-lg text-center delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Lihat Selengkapnya</a>
+        <div class="flex flex-col gap-2 xl:gap-3 2xl:gap-4">
+            <h2 class="text-lg md:text-xl lg:text-lg xl:text-3xl font-medium delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Selamat datang di</h2>
+            <h1 class="text-5xl md:text-6xl lg:text-5xl xl:text-[64px] 2xl:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Pendekar Banyumas</h1>
+            <h3 class="text-sm md:text-xl lg:text-lg xl:text-[22px] 2xl:text-2xl delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Pendeteksian Kesehatan Mental Banyumas</h3>
+            <p class="text-sm md:text-lg lg:text-base xl:text-lg 2xl:text-2xl font-semibold mt-5 delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">"Bersama Mewujudkan Jiwa yang Sehat"</p>
+            <a href="#about" class="bg-transparent bg-clip-border bg-gradient-to-r from-accent to-secondary text-white mt-8 2xl:py-4 py-2 px-2 rounded-[30px] w-40 md:w-45 md:h-10 2xl:h-15 2xl:w-60 text-xs md:text-sm 2xl:text-lg text-center delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">Lihat Selengkapnya</a>
         </div>
         <div>
-            <img src="{{ asset('assets/img/img-hero.png') }}" alt="image hero" class="w-115 2xl:w-[600px] mt-5 delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">
+            <img src="{{ asset('assets/img/img-hero.png') }}" alt="image hero" class="w-115 lg:w-96 xl:w-[450px] 2xl:w-[600px] mt-5 delay-[300ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="0">
         </div>
     </div>
 </section>
--- resources/views/client/page/homepage/components/tes.blade.php
+++ resources/views/client/page/homepage/components/tes.blade.php
@@ -1,28 +1,28 @@
 <section id="test" class="flex justify-center items-center h-[100vh]">
     <div class="flex flex-col justify-center items-center px-4 md:mx-12 lg:w-full">
         <div class="flex flex-col items-center gap-2 text-center">
-            <p class="lg:text-2xl font-semibold delay-[400ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">Mulai Tes</p>
-            <h1 class="text-2xl md:text-3xl lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary delay-[500ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">Pendeteksian Kesehatan Mental Sekarang</h1>
+            <p class="xl:text-xl2xl:text-2xl font-semibold delay-[400ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">Mulai Tes</p>
+            <h1 class="text-2xl md:text-3xl xl:text-[40px] 2xl:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary delay-[500ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">Pendeteksian Kesehatan Mental Sekarang</h1>
         </div>
         <div class="mt-6">
             <div class="flex flex-col lg:flex-row justify-center items-center gap-5 mt-5">
-                <div class="relative flex flex-col justify-between gap-5 lg:w-2/4 md:h-56 lg:h-70 bg-gradient-to-t from-soft to-white p-5 rounded-[30px] overflow-hidden shadow-md delay-[600ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">
+                <div class="relative flex flex-col justify-between gap-5 2xl:w-2/4 md:h-56 2xl:h-70 bg-gradient-to-t from-soft to-white p-5 rounded-[30px] overflow-hidden shadow-md delay-[600ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">
                     <div class="absolute w-10 h-70 bg-accent -left-5 -top-2">
                     </div>
-                    <div class="flex flex-col justify-center mt-3 md:mt-5 items-center gap-5">
-                        <h1 class="text-xl lg:text-3xl font-semibold">Screening Tes</h1>
-                        <p class="text-xs md:text-sm lg:text-lg text-center px-5 md:px-10 lg:px-25">Ikuti tes skrining di daerah Anda untuk mendapatkan rujukan secara langsung oleh Dinas Kesehatan Banyumas</p>
-                        <a href="" class="w-30 lg:w-40 lg:h-12 lg:text-lg py-2 bg-gradient-to-r from-accent to-secondary text-sm text-center text-white rounded-[30px]">Ikuti Tes</a>
+                    <div class="flex flex-col justify-center mt-3 md:mt-5 lg:mt-3 2xl:mt-5 items-center gap-5">
+                        <h1 class="text-xl 2xl:text-3xl font-semibold">Screening Tes</h1>
+                        <p class="text-xs md:text-sm 2xl:text-lg text-center px-5 md:px-10 xl:px-25">Ikuti tes skrining di daerah Anda untuk mendapatkan rujukan secara langsung oleh Dinas Kesehatan Banyumas</p>
+                        <a href="" class="w-30 2xl:w-40 2xl:h-12 2xl:text-lg py-2 bg-gradient-to-r from-accent to-secondary text-sm text-center text-white rounded-[30px]">Ikuti Tes</a>
                     </div>
                     <img src="{{ asset('assets/icon/elips.png') }}" alt="" class="w-30 absolute -right-1 -bottom-2">
                 </div>
-                <div class="relative flex flex-col justify-between gap-5 lg:w-2/4 md:h-56 lg:h-70 bg-gradient-to-t from-soft to-white p-5 rounded-[30px] overflow-hidden shadow-md delay-[600ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">
+                <div class="relative flex flex-col justify-between gap-5 2xl:w-2/4 md:h-56 2xl:h-70 bg-gradient-to-t from-soft to-white p-5 rounded-[30px] overflow-hidden shadow-md delay-[600ms] duration-[600ms] taos:scale-[0.6] taos:opacity-0 [animation-iteration-count:infinite]" data-taos-offset="100">
                     <div class="absolute w-10 h-70 bg-hard -left-5 -top-2">
                     </div>
-                    <div class="flex flex-col justify-center mt-3 md:mt-5 items-center gap-5">
-                        <h1 class="text-xl lg:text-3xl font-semibold">Tes Mandiri</h1>
-                        <p class="text-xs md:text-sm lg:text-lg text-center px-5 md:px-10 lg:px-25">Lakukan tes mandiri untuk mengetahui kondisi kesehatan mental Anda yang mungkin butuh perhatian lebih lanjut.</p>
-                        <a href="" class="w-30 lg:w-40 lg:h-12 lg:text-lg py-2 bg-gradient-to-r from-accent to-secondary text-sm text-center text-white rounded-[30px]">Ikuti Tes</a>
+                    <div class="flex flex-col justify-center mt-3 md:mt-5 lg:mt-3 2xl:mt-5 items-center gap-5">
+                        <h1 class="text-xl 2xl:text-3xl font-semibold">Tes Mandiri</h1>
+                        <p class="text-xs md:text-sm 2xl:text-lg text-center px-5 md:px-10 xl:px-25">Lakukan tes mandiri untuk mengetahui kondisi kesehatan mental Anda yang mungkin butuh perhatian lebih lanjut.</p>
+                        <a href="" class="w-30 2xl:w-40 2xl:h-12 2xl:text-lg py-2 bg-gradient-to-r from-accent to-secondary text-sm text-center text-white rounded-[30px]">Ikuti Tes</a>
                     </div>
                     <img src="{{ asset('assets/icon/elips.png') }}" alt="" class="w-30 absolute -right-1 -bottom-2">
                 </div>
